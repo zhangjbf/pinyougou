0ecfeed3195bb8d443191f989ba0df1f9db83ac1
jiabin.zhang <jiabin.zhang@rograndec.com>
2019-05-04 14:39:06 +0800

    dd

---
+++ pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/SellerController.java
@@ -0,0 +1,21 @@
+package com.pinyougou.shop.controller;
+
+import com.pinyougou.model.WebAppResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Version: 1.0
+ * @Author: jiabin.zhang 张佳宾
+ * @Email: jiabin.zhang@rograndec.com
+ * @CreateDate 2019/5/4
+ */
+@RestController
+@RequestMapping("/seller")
+public class SellerController {
+
+    @RequestMapping("/add")
+    public WebAppResult add(){
+        
+    }
+}
